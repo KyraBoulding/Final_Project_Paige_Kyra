6bd703ab19d417bfb721c19bb0d89a34bb90610a
KyraBoulding <45407769+KyraBoulding@users.noreply.github.com>
2018-12-17 23:55:25 -0800

    Update MainScript.R

--- MainScript.R
+++ MainScript.R
@@ -4,15 +4,83 @@
 
 ################################################################################
 
+################################################################################
+#
+#   This Script contains the work-flow set up, Libraries, Packages and the 
+#   data sets required to run this analysis.
+#   The scripts in this repo should be run in the following order:
+#
+#           - Main Script
+#           - Data.Cleaning.1
+#           - Analysis.Network
+#           - Analysis.MCA
+#
+#   All Scripts are located outside of the folders in the main working 
+#   directory of the repo. The folders contained in this work-flow are as 
+#   follows:
+#
+#           - 1.Raw.Data  -> the path to this folder is: path.rd
+#                 (This folder contains a copy of the original downloaded
+#                   data set without alterations.)
+#
+#           - 2.Clean.Data   -> the path to this folder is: path.cd
+#                 (This folder contains all subsets of data as well as the
+#                   primary cleaned data set.)
+#
+#           - 3.Analysis   -> the path to this folder is: path.a
+#                 (This folder contains saved out puts of our anaylsis.)
+#
+#           - 4.Graphs   -> the path to this folder is: path.g
+#                 (This folder contains all visualizeations of data.)
+#
+#               - Table   -> the path to this folder is: path.t
+#                   ( * THIS FOLDER IS LOCATED WITHIN THE 4.Graph FOLDER *
+#                     It contains all data out put as tables.)
+#
+################################################################################
 
 
+#=============================== PACKAGES ======================================
+
+# These are all packages used that are not in base R.
+
+install.packages("tidyverse")
+install.packages("igraph")
+install.packages("networkD3")
+install.packages("ggplot2")
+install.packages("readxl")
+install.packages("dplyr")
+install.packages("reshape2")
+install.packages(c("FactoMineR", "factoextra"))
 
 
+#=============================== LIBRARIES =====================================
+
+library(tidyverse)
+library(readxl)
+library(ggplot2)
+library(dplyr)
+library(grid)
+library(gridExtra)
+library(igraph)
+library(networkD3)
+library(reshape2)
+library(FactoMineR, factoextra)
+
+#=============================== DATA DOWNLOAD =================================
+
+# The file of the raw data can be accessed with the following code.
+
+getwd()  #should be set to the main "Final_Project_Paige_Kyra" folder by default
+setwd("1.Raw.Data")  # for some reason this is the only way it lets us get it...
+data <- read_xlsx("dataset_information.xlsx")
+setwd("~/GitHub/Final_Project_Paige_Kyra") #change the directory back to original
 
 
+#============================== VARIABLES ======================================
 
-
-
+# "Alpha.val" is the p-value threshold of significance 
+Alpha.val <- 0.05
 
 
 
